4e87584a7102df177142811392ecbb984d26490a
danntapl <31157096+danntapl@users.noreply.github.com>
2021-08-16 18:52:56 +0200

    segundo

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -3,6 +3,11 @@ import "./widgetLg.css";
 
 
 export default function WidgetLg() {
+    const Button = ({type})=>{
+        return(<button className={"widgetLgButton " + type}>{type} </button>);
+
+    }
+
     return (
         <div className="widgetLg">
             <h3 className="widgetLgTitle">Latest transactions</h3>
@@ -15,7 +20,55 @@ export default function WidgetLg() {
                    
                 </tr>
                 <tr className="widgetLgTr">
-                    <td className="widgetLgUser"></td>
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Carol Perez</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmoung">$122.1</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Approved"/>
+                    </td>
+
+                </tr>
+
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Carol Perez</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmoung">$122.1</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Declined"/>
+                    </td>
+
+                </tr>
+
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Carol Perez</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmoung">$122.1</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Pending"/>
+                    </td>
+
+                </tr>
+
+
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Carol Perez</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmoung">$122.1</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Aproved"/>
+                    </td>
 
                 </tr>
             </table>
